a68a93596a0075c4867f97167d014491f82aeab7
JORGE LUIS LUIS <li353029@uaeh.edu.mx>
2020-11-23 23:26:20 +0000

    final matriz inversa

--- Main.java
+++ Main.java
@@ -3,25 +3,36 @@ class Main {
 
   public static void Autores()
     {
+        System.out.println("Este programa fue hecho por:");
         System.out.println("");
-        System.out.println("Jorge Licea "  + "\nLuis Hernandes" + "\nOscar Morales");
-        System.out.println();
+        System.out.println(">> Jorge Licea "  + "\n>> Luis Hernandez" + "\n>> Oscar Morales");
+        System.out.println("");
     }
 
   public static void main(String[] args)
    {
       Autores();
-        Scanner leer=new Scanner(System.in);
-        System.out.println("Numero de filas ");
-        int fil=leer.nextInt();
-        System.out.println("Numero de columnas ");
-        int col=leer.nextInt();
+        Scanner leer = new Scanner(System.in);
+        System.out.println("Numero de filas: ");
+        System.out.print("");
+        int fil = leer.nextInt();
+        System.out.println("Numero de columnas: ");
+        System.out.print("");
+        int col = leer.nextInt();
 
         
         //crear objeto Matriz
         Matriz mat1 = new Matriz(fil, col);
         mat1.llenarMat();
-        mat1.mostrarMat();
+        System.out.println("");
+        mat1.mostrarMat(mat1.x);
+        System.out.println("");
         mat1.signo();
+        System.out.println("\nEl determinante de la matriz es: " + mat1.determinante(mat1.x));  
+        System.out.println("La matriz de cofactores es: ");
+        double [][] matC = mat1.matrizCofactores(mat1.x);
+        mat1.mostrarMat(matC);    
+        System.out.println("\nLa matriz inversa es: \n");
+        mat1.inversa(matC);
   }
 }
--- Matriz.java
+++ Matriz.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import static java.lang.Math. *;
 public class Matriz
 {
     Scanner scan = new Scanner(System.in);
@@ -11,16 +12,17 @@ public class Matriz
         this.filas = filas;
         this.columnas = columnas;
         x = new double[filas][columnas];
+        System.out.println("");
         System.out.println("Creando una nueva matriz!!");
     }
     
-    public void mostrarMat()
+    public void mostrarMat(double mat [][])
     {
         for(int i = 0; i < this.filas; i++)
         {
             for(int j = 0; j < this.columnas; j++)
             {
-                System.out.print(this.x[i][j] + " |");
+                System.out.print("| " + mat[i][j] + " | ");
             }
             System.out.println("");
         }
@@ -36,6 +38,7 @@ public class Matriz
                 x[i][j] = scan.nextDouble();
             }
         }
+        System.out.print("");
     }
 
     public void signo()
@@ -44,7 +47,7 @@ public class Matriz
         {
             for(int j=0;j<columnas;j++)
             {
-                if(x[i][j]%2==0)
+                if(Math.pow(-1,(i+j)) > 0)
                 {
                     System.out.print("+ ");
                 }
@@ -56,5 +59,123 @@ public class Matriz
             System.out.println("");
         }
     }
+
+    public double determinante (double [][] matriz)
+	{
+		double determinante = 0 ;
+		
+		int fil = matriz.length;
+		int col = matriz[0].length;
+		
+		// Si la matriz es 1x1, el determinante es el elemento de la matriz
+		if ((fil==1) && (col==1))
+                {
+			return matriz[0][0];
+                }
+
+		int signo=1;
+		
+		for (int columna=0;columna<col;columna++)
+		{
+			// Obtiene el adjunto de fila=0, columna=columna, pero sin el signo.
+      //submatriz Se vuelve una matriz
+			double[][] submatriz = getSubmatriz(matriz, fil, col,columna);
+			determinante = determinante + signo*matriz[0][columna]*determinante(submatriz);
+			signo*=-1;
+		}
+		
+		return determinante;
+	}
+
+	
+	public double[][] getSubmatriz(double[][] matriz, 
+			int fil,
+			int col, 
+			int columna) 
+      {
+		double [][] submatriz = new double[fil-1][col-1];
+		int con=0;
+		for (int j=0;j<col;j++)
+		{
+			if (j==columna) 
+      continue;
+			for (int i=1;i<fil;i++)
+      {
+      	submatriz[i-1][con]=matriz[i][j];
+      }
+        con++;
     
+    }
+		return submatriz;
+	}
+
+  public double[][] transpuesta(double[][] matriz)
+  {
+    double trans[][]=new double[matriz[0].length][matriz.length];
+    for(int i=0;i<matriz.length;i++)
+    {
+      for(int j=0;j<matriz.length;j++)
+      {
+        trans[i][j]=matriz[j][i];
+       System.out.print("| "+trans[i][j]+" |");
+      }
+      System.out.println(" ");
+    }
+    return trans;
+  }
+  
+  public double[][] matrizCofactores(double[][] matriz){
+    double[][] nm=new double[matriz.length][matriz.length];
+    for(int i=0;i<matriz.length;i++) {
+      for(int j=0;j<matriz.length;j++) {
+        double[][] det=new double[matriz.length-1][matriz.length-1];
+  double detValor;
+      for(int k=0;k<matriz.length;k++) 
+      {
+        if(k!=i) 
+        {
+          for(int l=0;l<matriz.length;l++) 
+          {
+            if(l!=j) 
+            {
+              int indice1=k<i ? k : k-1;
+              int indice2=l<j ? l : l-1;
+              det[indice1][indice2]=matriz[k][l];
+            }
+          }
+        }
+      }
+         detValor = deter(det);
+         nm[i][j]= detValor * (double)Math.pow(-1,i+j+2);
+      }
+    }
+  return nm;
+}
+
+public static double deter(double mat [][])
+{
+  double det = 0;
+  if(mat.length == 2)
+  {
+  det = (mat[0][0] * mat[1][1]) - (mat[0][1] * mat[1][0]);
+  }
+  if(mat.length == 3)
+  {
+  det= (mat[0][0]*mat[1][1]*mat[2][2]) + (mat[1][0]*mat[2][1]*mat[0][2]) + (mat[2][0] * mat[0][1] * mat[1][2]) - ((mat[0][2] + mat [1][1] + mat[2][0]) + (mat[1][2] + mat[2][1] + mat[0][0]) + (mat[2][2] + mat[0][1] + mat[1][0]));
+  }
+  return det;
+}
+public void inversa(double mat[][])
+{
+  for(int i=0;i<x.length;i++)
+  {
+    for(int j=0;j<x[i].length;j++)
+    {
+      System.out.print(mat[i][j]+"/"+ determinante(x) + "  |");
+    }
+    System.out.println();
+  }
+   
+}
+
 }
